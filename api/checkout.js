e6aac1e992777433fa91dc5337d8112971a219d8
professordavoz <professordavozkadu@gmail.com>
2026-03-13 14:27:46 -0300

    Update checkout.js

--- api/checkout.js
+++ api/checkout.js
@@ -33,7 +33,6 @@ export default async function handler(req, res) {
 
         const data = await response.json();
         return res.status(200).json(data);
-
     } catch (err) {
         return res.status(500).json({ error: err.message });
     }
